e97f73f1e350607db13a9034ba3493c79649547d
Neha Gupta <er.nehaa.gupta@gmail.com>
2020-02-04 14:54:37 -0500

    Setting up the express server

---
+++ client/build/precache-manifest.2421bcc66e06c5f3a6d6ee863bd5a4a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "667671313dd50fd0689b",
+    "url": "/static/css/main.e9c80b7d.chunk.css"
+  },
+  {
+    "revision": "667671313dd50fd0689b",
+    "url": "/static/js/main.79760d35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3070b4aec8da0e6e7adb",
+    "url": "/static/js/2.a9a10943.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "a349ded21032cc94cf0eb0a80d385295",
+    "url": "/index.html"
+  }
+];
