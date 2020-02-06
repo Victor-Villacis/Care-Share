e1fa7ae799611392f805b587cbee66fd5cad7301
Cibelle Montor <you@example.com>
2020-02-05 19:17:26 -0700

    styled nav bar component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,28 @@
 import React from "react";
-
+import "./style.css";
 function Nav() {
   return (
-    <nav>
+    <nav className="nav-logout-view">
       <div class="nav-wrapper">
-        <a href="#" class="brand-logo">
-          Logo
+        <a href="https://montclaircareshare.com/">
+          <img
+            id="logo"
+            src={require("./images/mcslogo.jpg")}
+            alt="MontClair Care Share"
+          />
         </a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <ul id="nav-mobile" className="right hide-on-med-and-down nav-links">
           <li>
-            <a href="sass.html">Sass</a>
+            <a href="#">About</a>
           </li>
           <li>
-            <a href="badges.html">Components</a>
+            <a href="#">News</a>
           </li>
           <li>
-            <a href="collapsible.html">JavaScript</a>
+            <a href="#">Contact us</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
           </li>
         </ul>
       </div>
